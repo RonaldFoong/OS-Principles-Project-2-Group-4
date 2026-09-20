eaca058dc94e493b121c1929e636a0c0af36e9bc
Ronald Foong <ronaldfoong25@gmail.com>
2026-09-20 16:43:42 +1000

    Update memlib.c

--- memlib.c
+++ memlib.c
@@ -1,6 +1,7 @@
 #include <stddef.h>
 #include "alloc_list.h"
 
+// Global Variables
 alloc_list_t occupied_chunks;
 alloc_list_t free_chunks;
 
@@ -9,17 +10,5 @@ void *alloc(size_t chunk_size) {
 }
 
 void *dealloc(void *chunk) {
-
-}
-
-void *firstfit(alloc_list_t *list) {
-
-}
-
-void *bestfit(alloc_list_t *list) {
-
-}
-
-void *quickfit(alloc_list_t *list) {
 
 }
