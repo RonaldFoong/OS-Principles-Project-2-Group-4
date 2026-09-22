cb50c61b968d4d3a49f0c81bbd7337ea8065194b
Ronald Foong <ronaldfoong25@gmail.com>
2026-09-22 14:11:21 +1000

    Added destructor for alloc_list_t

--- utils/alloc_list.c
+++ utils/alloc_list.c
@@ -36,3 +36,14 @@ allocation_t *pop(alloc_list_t *self) {
     free(node);
     return popped;
 }
+
+void alloc_list_destroy(alloc_list_t *self) {
+    alloc_node_t *node = self->head;
+    alloc_node_t *next = NULL;
+    while (node != NULL) {
+        free(node->alloc);
+        next = node->next;
+        free(node);
+        node = next;
+    }
+}
